2cef26a9e609652c8acda2e6743a0832ec0dc6e2
Rey Santana Socorro <58710601+reysantana@users.noreply.github.com>
2021-06-05 03:58:28 -0500

    added some temporary models

--- src/js/plate.js
+++ src/js/plate.js
@@ -2,13 +2,21 @@ const PlateComponent = {
 
     init() {
         const scene = document.querySelector('a-scene');
-        this.raycaster = new THREE.Raycaster()
-        this.camera = document.getElementById('camera')
-        this.threeCamera = this.camera.getObject3D('camera')
-        this.ground = document.getElementById('ground')
-        this.cursor = document.getElementById('cursor')
-        this.rayOrigin = new THREE.Vector2(0, 0)
-        this.cursorLocation = new THREE.Vector3(0, 0, 0)
+        this.raycaster = new THREE.Raycaster();
+        this.camera = document.getElementById('camera');
+        this.threeCamera = this.camera.getObject3D('camera');
+        this.ground = document.getElementById('ground');
+        this.cursor = document.getElementById('cursor');
+        this.rayOrigin = new THREE.Vector2(0, 0);
+        this.cursorLocation = new THREE.Vector3(0, 0, 0);
+        this.menuItems = document.getElementsByClassName("menu-item");
+        for (let i = 0; i < this.menuItems.length; i++) {
+            const menuItem = this.menuItems[i];
+            const menuId = menuItem.getAttribute('menu-id');
+            menuItem.addEventListener('click', () => {
+                this.placeMeal(menuId);
+            });
+        }
 
         if (scene.hasLoaded) {
             //this.onLoadComplete()
@@ -16,22 +24,75 @@ const PlateComponent = {
         } else {
             scene.addEventListener('loaded', () => {
                 //this.onLoadComplete()
-                console.log("scene load complete")
+                console.log("scene load complete");
             })
         }
     },
+
     tick() {
-        // Raycast from camera to 'ground'
-        this.raycaster.setFromCamera(this.rayOrigin, this.threeCamera)
-        const intersects = this.raycaster.intersectObject(this.ground.object3D, true)
+        this.raycaster.setFromCamera(this.rayOrigin, this.threeCamera);
+        const intersects = this.raycaster.intersectObject(this.ground.object3D, true);
         if (intersects.length > 0) {
-            const [intersect] = intersects
-            this.cursorLocation = intersect.point
+            const [intersect] = intersects;
+            this.cursorLocation = intersect.point;
         }
-        this.el.object3D.position.y = 0.1
-        this.el.object3D.position.lerp(this.cursorLocation, 0.4)
-        this.el.object3D.rotation.y = this.threeCamera.rotation.y
+        this.el.object3D.position.y = 0.1;
+        this.el.object3D.position.lerp(this.cursorLocation, 0.4);
+        this.el.object3D.rotation.y = this.threeCamera.rotation.y;
     },
+
+    placeMeal(id) {
+        this.cursor.setAttribute('visible', false);
+
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.classList.remove('open');
+        sidebar.classList.add('close');
+
+        const plateSpawned = document.getElementById('plateSpawned')
+        if (plateSpawned != undefined) {
+            plateSpawned.remove();
+        }
+
+        const newMeal = document.createElement('a-entity');
+        newMeal.setAttribute('id', 'plateSpawned');
+        newMeal.setAttribute('position', this.el.object3D.position);
+        newMeal.setAttribute('rotation', this.el.object3D.rotation);
+        newMeal.setAttribute('scale', '1 1 1');
+        newMeal.setAttribute('xrextras-hold-drag', '');
+        newMeal.setAttribute('xrextras-two-finger-rotate', '');
+        newMeal.classList.add('cantap');
+        newMeal.setAttribute('gltf-model', '../resources/models/' + id + '.glb');
+        newMeal.setAttribute('shadow', {
+            receive: false
+        });
+        newMeal.setAttribute('visible', false);
+        this.el.sceneEl.appendChild(newMeal);
+        newMeal.addEventListener('model-loaded', () => {
+            newMeal.setAttribute('animation-mixer', {
+                loop: 'repeat',
+                crossFadeDuration: 0.4,
+                clampWhenFinished: true
+            });
+            newMeal.setAttribute('visible', true);
+        });
+    }
+}
+
+window.closeMenu = (e) => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.remove('open');
+    sidebar.classList.add('close');
+}
+
+window.toggleMenu = (e) => {
+    const sidebar = document.querySelector('.sidebar');
+    if (sidebar.classList.contains('open')) {
+        sidebar.classList.add('close');
+        sidebar.classList.remove('open');
+    } else {
+        sidebar.classList.remove('close');
+        sidebar.classList.add('open');
+    }
 }
 
 export {
